e5c8c1cf34e669a18a81900cf449fedd6e3a1788
Hachaika <113956176+Hachaika@users.noreply.github.com>
2022-12-27 19:20:30 +0700

    Корзина

---
+++ Lab9.js
@@ -0,0 +1,112 @@
+function clickMe(button) {
+    if(button.innerHTML == 'Щелчок')
+        button.innerHTML = 'Спасибо';
+    else
+    button.innerHTML = 'Щелчок'
+}
+
+function student(button) {
+    if(button.innerHTML == 'Поляков')
+        button.innerHTML = 'Кирилл';
+    else
+    button.innerHTML = 'Поляков'
+}
+
+function student2(button) {
+    if(button.innerHTML == 'Мищенко')
+        button.innerHTML = 'Владислав';
+    else
+    button.innerHTML = 'Мищенко'
+}
+
+function knopka(button) {
+    if(button.innerHTML == 'Нажми меня') {
+        button.innerHTML = 'Ещё раз нажми';
+        button.style.color = 'purple';
+    }
+        
+    else if(button.innerHTML == 'Ещё раз нажми') {
+        button.innerHTML = 'Ещё';  
+        button.style.color = 'green';
+    }
+         
+    else if(button.innerHTML == 'Ещё') {
+        button.innerHTML = 'Начать сначала';   
+        button.style.color = 'orange';
+    }
+        
+    else {
+        button.innerHTML = 'Нажми меня' 
+        button.style.color = 'yellow'; 
+    }
+}
+
+function showCoords(event) {
+    event.target.innerHTML = 'x = ' + event.offsetX + ' ' + ', y = ' + event.offsetY + ' ' + (event.ctrlKey? '<b>Ctrl</b>':'<s>Ctrl</s>') + ' ' + (event.shiftKey? '<b>Shift</b>':'<s>Shift</s>') + ' ' + (event.altKey? '<b>Alt</b>':'<s>Alt</s>' );
+}
+window.addEventListener('DOMContentLoaded', function() {
+    let coords = document.querySelector('#coords');
+    coords.addEventListener('mousemove',  showCoords);
+});
+
+function showFocus(event) {
+    event.target.style.color = 'purple';
+    event.target.style.fontSize = '16px';
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    let texts = document.querySelectorAll('input[type = "text"]');
+    texts[0].addEventListener('focus',  showFocus);
+    texts[1].addEventListener('focus',  showFocus);
+    texts[2].addEventListener('focus',  showFocus);
+});
+
+function showBlur(event) {
+    event.target.style.color = 'green';
+    event.target.style.fontSize = '20px';
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    let texts = document.querySelectorAll('input[type = "text"]');
+    texts[0].addEventListener('blur',  showBlur);
+    texts[1].addEventListener('blur',  showBlur);
+    texts[2].addEventListener('blur',  showBlur);
+});
+
+function showChange(event) {
+    document.getElementById('output').innerHTML=event.target.value;
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    let texts = document.querySelectorAll('input[type = "text"]');
+    texts[0].addEventListener('change',  showChange);
+    texts[1].addEventListener('change',  showChange);
+    texts[2].addEventListener('change',  showChange);
+});
+
+function showChange2(event) {
+    document.getElementById('output2').innerHTML=event.target.value;
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    let texts = document.querySelectorAll('input[type = "number"]');
+    texts[0].addEventListener('change',  showChange2);
+});
+
+function showChange3(event) {
+    document.getElementById('output3').innerHTML=event.target.value;
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    let texts = document.querySelectorAll('input[type = "number"]');
+    texts[1].addEventListener('change',  showChange3);
+});
+
+function showChange4(event) {
+    document.getElementById('output4').innerHTML=event.target.value;
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    let texts = document.querySelectorAll('input[type = "number"]');
+    texts[2].addEventListener('change',  showChange4);
+});
